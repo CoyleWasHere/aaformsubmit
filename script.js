24cd31e0be2450f644dca4c807b4ad50310396b4
Daniel Coyle <64401148+CoyleWasHere@users.noreply.github.com>
2023-07-11 11:00:00 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -80,3 +80,11 @@ function removeInput(){
 
     number -= 1
   }
+
+
+  function customMessage() {
+
+    const poNumber = document.getElementById("PO")
+
+    return "Thank You For Your Order, Use " + poNumber + " To Track This Order"
+  }
